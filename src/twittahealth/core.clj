38b1f4fa91536038952e8fd6fc642c2c934ec479
Shah Sidi <ssidi@yahoo.com>
2014-10-25 16:09:28 -0700

    "Shah - POC for sending and listening heartbeat"

--- src/twittahealth/core.clj
+++ src/twittahealth/core.clj
@@ -7,28 +7,71 @@
             [clojure.core.async :as async]
             [environ.core :refer [env]]
             [clojure.tools.logging :as log]
-            ))
+            )
+  (:import
+    (com.twitter.hbc ClientBuilder)
+    (com.twitter.hbc.core Client Constants)
+    (com.twitter.hbc.core.endpoint StatusesFilterEndpoint Location Location$Coordinate)
+    (com.twitter.hbc.core.processor StringDelimitedProcessor)
+    (com.twitter.hbc.httpclient.auth Authentication OAuth1)
+    (java.util.concurrent BlockingQueue LinkedBlockingQueue))
+  )
 
 ;creds for twitter-api calls - read from environment
-(def my-creds (toauth/make-oauth-creds (env :twitter-api-key)
-                (env :twitter-api-secret)
-                (env :twitter-user-token)
-                (env :twitter-user-secret)))
+(def my-creds (toauth/make-oauth-creds (env :twitta-health-api-key)
+                (env :twitta-health-api-secret)
+                (env :twitta-health-user-token)
+                (env :twitta-health-user-secret)))
 
-(defn health-check
-  []
+(def health-tag "#twittahealth")
+(def health-match-tag ["#twittahealth"])
+
+(defn send-heartbeat []
   (try
     (let [result (tapi/statuses-update :oauth-creds my-creds
-                   :params {:status "#twittahealth3"})]
+                   :params {:status (str health-tag " " (rand-int Integer/MAX_VALUE))})]
       (log/debug "twitter status update result: " result))
     (catch Exception e
-      (log/error "Error creating tweet/status update (exception, result): ")
+      (log/error "Error creating tweet/status update for: " health-tag)
       (log/error e)
-;      (log/error result)))
   )))
 
+;generate auth object - creds for hbc calls
+(def consumer-key (env :twitter-api-key) )
+(def consumer-secret (env :twitter-api-secret))
+(def token (env :twitter-user-token))
+(def secret (env :twitter-user-secret))
+(def auth (OAuth1. consumer-key consumer-secret token secret))
+;create streaming endpoint filter
+(def endpoint (StatusesFilterEndpoint.))
 
-(defn -main
-  "twitts #teittahealth"
-  []
-  (health-check))
+(defn create-client []
+
+  ;set up a queue to store messages in
+  (def queue (LinkedBlockingQueue. 10000))
+
+  ;create a list of all track terms from all campaigns and use for twitter listening
+  (. endpoint trackTerms health-match-tag)
+
+  ;build the client
+  (def client (-> (ClientBuilder.)
+                (. hosts (. Constants STREAM_HOST))
+                (. endpoint endpoint)
+                (. authentication auth)
+                (. processor (StringDelimitedProcessor. queue))
+                (. build)))
+  )
+
+(defn listen-for-heartbeat []
+    (create-client)
+
+    ;connect the client
+    (. client connect)
+
+    ;inifinite loop, take message off queue each iteration and process it
+    (log/info "Listening for heartbeat tweets...")
+    (loop []
+      (let [data (json/read-str (. queue take) :key-fn clojure.core/keyword)]
+        (println data)
+            (recur)))
+    (println "done listening"))
